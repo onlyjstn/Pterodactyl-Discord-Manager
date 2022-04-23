440d7810731f17f94018be6199e6b8e75ad864dc
Finn Wolf <66327503+Einkornwolf@users.noreply.github.com>
2022-04-23 16:30:47 +0200

    Beautify

--- regcommands.js
+++ regcommands.js
@@ -1,12 +1,20 @@
 // Alle Imports und Requierements
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const {
+	SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+	REST
+} = require('@discordjs/rest');
+const {
+	Routes
+} = require('discord-api-types/v9');
 const fs = require('fs');
 const dotenv = require('dotenv')
 
 //Initialize DotEnv
-dotenv.config({ path: './config.env'})
+dotenv.config({
+	path: './config.env'
+})
 const token = process.env.BOT_TOKEN
 const clientid = process.env.BOT_CLIENT_ID
 const testserverid = process.env.BOT_SINGLE_SERVER_ID
@@ -22,19 +30,22 @@ for (const file of commandFiles) {
 
 //Initiere Slash Commands und schicke sie an die Discord API via REST
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({
+	version: '9'
+}).setToken(token);
 
 (async () => {
-    try {
-        console.log('Ich lese nun alle Slash-Commands neu aus und versende die an alle verfügbaren Server... dies kann einen Moment dauern!');
+	try {
+		console.log('Ich lese nun alle Slash-Commands neu aus und versende die an alle verfügbaren Server... dies kann einen Moment dauern!');
 
-        await rest.put(
-            Routes.applicationGuildCommands(clientid, testserverid),
-            {body: slashcommands },
-        );
+		await rest.put(
+			Routes.applicationGuildCommands(clientid, testserverid), {
+				body: slashcommands
+			},
+		);
 
-        console.log('Ich habe alles erledigt und die Slash Commands wurden aktualisiert!');
-    } catch (err) {
-        console.log('Uppss... da ist wohl ein Fehler aufgetreten: ' + err);
-    }
+		console.log('Ich habe alles erledigt und die Slash Commands wurden aktualisiert!');
+	} catch (err) {
+		console.log('Uppss... da ist wohl ein Fehler aufgetreten: ' + err);
+	}
 })();
