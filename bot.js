a95613e7c93a7b113514eb95415db22c818ac28c
Finn Wolf <66327503+Einkornwolf@users.noreply.github.com>
2022-10-18 22:43:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,81 +1,44 @@
 // Alle requires und Requierements
 const dotenv = require('dotenv');
+const path = require('node:path');
+const axios = require('axios')
+const { GlobalFonts }  = require("@napi-rs/canvas");
+const { join } = require('path');
 
 //Initializte DotEnv
 dotenv.config({
 	path: './config.env'
 })
-
-const {
+const {	
 	GatewayIntentBits,
 	Partials
 } = require('discord.js');
 const Discord = require('discord.js');
-const client = new Discord.Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
-	partials: [Partials.Channel]
+const { ManagerClient } = require("./managers/manager")
+const client = new ManagerClient({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers],
+	partials: [Partials.Channel, Partials.User, Partials.GuildMember, Partials.ThreadMember]
 });
-const fs = require('fs');
 const {
-	Client,
 	Collection,
-	Intents
 } = require('discord.js');
-const clientid = process.env.BOT_CLIENT_ID
 const token = process.env.BOT_TOKEN
-const system = require('./functions');
 
+//Load Global Fonts
+GlobalFonts.registerFromPath(join(__dirname, 'fonts', 'impact.ttf'), 'impact')
 
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-//Client Event Handler
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-//Client Command Export Handler
-
+//Set Event Listener Maximum to Infinity :)
+client.setMaxListeners(Infinity)
+//Create Collections
 client.commands = new Collection()
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	client.commands.set(command.data.name, command)
-}
+client.events = new Collection()
+client.buttons = new Collection()
+client.selectMenus = new Collection()
+client.modals = new Collection()
+client.analogCommands = new Collection()
+client.cronJobs = new Collection()
 
-//Verwende und Antworte auf Slash-Commands
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.log(error)
-		await interaction.channel.send({
-			content: 'Tut mir leid! Während ich veruscht habe diesen Command auszuführen ist mir ein Fehler unterlaufen... Probiere es gerne noch einmal.',
-			ephemeral: true
-		});
-	}
-});
-
-
-//Startup Log
-
-client.on('ready', () => {
-	console.log('Hallo! Ich wurde erfolgreich eingeloggt als folgender Client: ' + client.user.tag + '!');
-});
-
-
-
+//Load Events // Client Event Handler
+client.loadEvents()
 //Login Bot
 client.login(token);
