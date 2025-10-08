f0f6c023ea971357be4c7e4c9c372b1a71848e32
Finn Wolf <einkornwolflp@gmail.com>
2025-10-08 20:18:16 +0200

    Emojis

--- classes/emojiManager.js
+++ classes/emojiManager.js
@@ -2,12 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 class EmojiManager {
+
+
+    /**
+     * 
+     * Provides a simple way to retreive either custom emojis via id or unicode based emojis if an id is not present
+     * 
+     */
+
   constructor(root = path.resolve(__dirname, '..')) {
     this.root = root;
     this.file = path.join(this.root, 'translations', 'emojis.json');
   }
 
-  // Very simple: read file each time, return id if present (non-empty), else emoji char, or null
   async getEmoji(key) {
     const txt = await fs.promises.readFile(this.file, 'utf8');
     const json = JSON.parse(txt);
