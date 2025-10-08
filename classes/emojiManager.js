5ddc1e93c9afaae89a9189c1ded33a7dc4bc4711
Finn Wolf <einkornwolflp@gmail.com>
2025-10-08 20:20:33 +0200

    Formatting

--- classes/emojiManager.js
+++ classes/emojiManager.js
@@ -1,22 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 class EmojiManager {
+  /**
+   *
+   * Provides a simple way to retreive either custom emojis via id or unicode based emojis if an id is not present
+   *
+   */
 
-
-    /**
-     * 
-     * Provides a simple way to retreive either custom emojis via id or unicode based emojis if an id is not present
-     * 
-     */
-
-  constructor(root = path.resolve(__dirname, '..')) {
+  constructor(root = path.resolve(__dirname, "..")) {
     this.root = root;
-    this.file = path.join(this.root, 'translations', 'emojis.json');
+    this.file = path.join(this.root, "translations", "emojis.json");
   }
 
   async getEmoji(key) {
-    const txt = await fs.promises.readFile(this.file, 'utf8');
+    const txt = await fs.promises.readFile(this.file, "utf8");
     const json = JSON.parse(txt);
     const entry = json[key];
     if (!entry) return null;
